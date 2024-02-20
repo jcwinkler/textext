2992dff846cc8dc829d3e0a2ab5b044c22f186c8
Jan Winkler <enceladus@posteo.de>
2024-02-20 10:58:12 +0100

    Update to final directory structure

--- v1/tests/win_app_paths/test_win_app_path.py
+++
@@ -1,9 +0,0 @@
-import os
-import sys
-
-sys.path.append(os.path.join("../../", "textext"))
-
-import win_app_paths as wap
-
-print("Installation directories of executables:")
-print(wap.get_non_syspath_dirs())
